689c3b7b63614ec79816c69f597fc8304a1d6520
lgw277216 <ligwhu@vip.qq.com>
2017-08-05 11:17:52 +0800

    css公共样式

--- application/pipacker/controller/Mainpage.php
+++ application/pipacker/controller/Mainpage.php
@@ -10,7 +10,7 @@
 		public function index()
 	    {
 	    	// print_r($this) ;
-	    	// 是去到view里面对应的login文件夹下面的index页面
+	    	// 是去到view里面对应的mainpage文件夹下面的index页面
 	    	return $this->fetch();
 	    }
 	}
