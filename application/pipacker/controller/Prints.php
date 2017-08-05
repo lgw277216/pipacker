4b7ba3184f81e7b5e58fffd40f4c5019422c925b
yehith <yehith@sina.com>
2017-08-05 10:53:42 +0800

    test

--- application/pipacker/controller/Prints.php
+++ application/pipacker/controller/Prints.php
@@ -9,9 +9,7 @@
 		
 		public function index()
 	    {
-	    	// print_r($this) ;
-	    	// 是去到view里面对应的login文件夹下面的index页面
-	    	return $this->fetch();
+	    	return $this->fetch('pgskill');
 	    }
 	}
  ?>
